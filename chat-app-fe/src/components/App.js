4eac317e2b16ecae06074738db547d93c723106e
Luis PM <lpm@pop-os.localdomain>
2020-12-19 15:37:51 -0500

    fixed bug on useLocalStorage,

--- chat-app-fe/src/components/App.js
+++ chat-app-fe/src/components/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Login from "./Login";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 function App() {
-  const [id, setid] = useState();
+  const [id, setid] = useLocalStorage("id");
   return (
     <>
       {id}
--- chat-app-fe/src/hooks/useLocalStorage.js
+++ chat-app-fe/src/hooks/useLocalStorage.js
@@ -1,19 +1,22 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-//use to distinguish localstorage keys
 const PREFIX = "whatsapp-clone-";
 
 export default function useLocalStorage(key, initialValue) {
   const prefixedKey = PREFIX + key;
-  const [value, setvalue] = useState(() => {
+  const [value, setValue] = useState(() => {
     const jsonValue = localStorage.getItem(prefixedKey);
-    if (jsonValue !== null) {
-      return JSON.parse(jsonValue);
-    }
+    if (jsonValue != null) return JSON.parse(jsonValue);
     if (typeof initialValue === "function") {
       return initialValue();
     } else {
       return initialValue;
     }
   });
+
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
+
+  return [value, setValue];
 }
