988cd6edc40b504dfc92d86d4fad78396ed056eb
Luis PM <lpm@pop-os.localdomain>
2020-12-21 04:44:31 -0500

    EOD push

--- chat-app-fe/src/components/App.js
+++ chat-app-fe/src/components/App.js
@@ -4,15 +4,18 @@ import useLocalStorage from "../hooks/useLocalStorage";
 import Dashboard from "./Dashboard";
 import { ContactsProvider } from "../contexts/ContactsProvider";
 import { ConversationsProvider } from "../contexts/ConversationProvider";
+import { SocketProvider } from "../contexts/SocketProvider";
 function App() {
   const [id, setid] = useLocalStorage("id");
 
   const dashboard = (
-    <ContactsProvider>
-      <ConversationsProvider id={id}>
-        <Dashboard id={id} />
-      </ConversationsProvider>
-    </ContactsProvider>
+    <SocketProvider id={id}>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
+      </ContactsProvider>
+    </SocketProvider>
   );
 
   return <>{id ? dashboard : <Login onIdSubmit={setid} />}</>;
