a47540acb45ec54161e6c81873d3a412f6b391cd
Luis PM <lpm@pop-os.localdomain>
2020-12-21 02:10:18 -0500

    init OpenConversation component to render converstion

--- chat-app-fe/src/components/Dashboard.js
+++ chat-app-fe/src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useConversations } from "../contexts/ConversationProvider";
+import OpenConversation from "./OpenConversation";
 import Sidebar from "./Sidebar";
 
 function Dashboard({ id }) {
+  const { selectedConversation } = useConversations();
   return (
     <div className="d-flex" style={{ minHeight: "100vh" }}>
       <Sidebar id={id} />
+      {selectedConversation && <OpenConversation />}
     </div>
   );
 }
